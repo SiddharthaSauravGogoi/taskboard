fe859cc77310186a444f5ba5fd800b823b12b894
SiddharthaSauravGogoi <lezaroth3@gmail.com>
2020-05-29 01:57:57 +0530

    Updates

---
+++ precache-manifest.37717c0421acc2eb7c14cc9f3b8b4630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eca86b4bff3666369f42925905acfd2",
+    "url": "/taskboard/index.html"
+  },
+  {
+    "revision": "bcad1c98164e9b03b41c",
+    "url": "/taskboard/static/css/main.95374084.chunk.css"
+  },
+  {
+    "revision": "991316ab3825168d19c8",
+    "url": "/taskboard/static/js/2.daa4573f.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/taskboard/static/js/2.daa4573f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcad1c98164e9b03b41c",
+    "url": "/taskboard/static/js/main.5f769f63.chunk.js"
+  },
+  {
+    "revision": "18632350eea341f61312",
+    "url": "/taskboard/static/js/runtime-main.3a9599c7.js"
+  }
+]);
